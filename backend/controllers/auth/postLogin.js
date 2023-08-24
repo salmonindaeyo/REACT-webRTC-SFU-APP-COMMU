af1ed1f0e0c0ebafbc40a4216db9946c06ec4224
salmonindaeyo <iamfhaa@hotmail.com>
2023-08-24 22:38:43 +0700

    controller + validate + first endpoint login , register

---
+++ backend/controllers/auth/postLogin.js
@@ -0,0 +1,6 @@
+const postLogin = async (req,res) => {
+    res.send("login route")
+}
+
+
+module.exports = postLogin
--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,33 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config();
 
+const authRoutes = require('./routes/authRoutes')
+
 const PORT = process.env.PORT || process.env.API_PORT
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+// register the routes
+app.use('/api/auth', authRoutes)
+
+
 const server = http.createServer(app);
 
-server.listen(PORT , ()=> {
-    console.log(`server is listening on ${PORT}`)
+console.log("start server")
+
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    server.listen(PORT , ()=> {
+        console.log(`server is listening on ${PORT}`)
+    });
+    
+})
+.catch(err => {
+    console.log("database connection failed. server not start")
+    console.log(err)
 })
