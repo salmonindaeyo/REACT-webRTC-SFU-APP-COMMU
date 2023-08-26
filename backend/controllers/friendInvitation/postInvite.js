51806d65075e1acb7583ecab2b4e02ffb9765f52
salmonindaeyo <iamfhaa@hotmail.com>
2023-08-26 18:12:29 +0700

    add friend on database

--- backend/controllers/friendInvitation/postInvite.js
+++ backend/controllers/friendInvitation/postInvite.js
@@ -24,9 +24,37 @@ const postInvite = async (req, res) => {
       }
 
       // check if invitation has been already sent
+      const invitationAlreadyReceived = await FriendInvitation.findOne({
+        senderId: userId,
+        receiverId: targetUser._id
+      })
 
+      if(invitationAlreadyReceived) {
+        return res.status(409).send("Invitation has been already sent");
+    }
 
-   return res.send("controller is working")
+    //check if the user which we would like to invite is already our friend
+    const usersAlreadyFriends = targetUser.friends.find(
+        (friendId) => friendId.toString() === userId.toString()
+      );
+      if (usersAlreadyFriends) {
+        return res
+          .status(409)
+          .send("Friend already added. Please check friends list");
+      }
+    
+
+       // create new invitation in database
+  const newInvitation = await FriendInvitation.create({
+    senderId: userId,
+    receiverId: targetUser._id,
+  });
+
+  // if invtiation has been successfully created we would like to update friends invitations if other user is online
+  // send pending invitations update to specific user
+//   friendsUpdates.updateFriendsPendingInvitations(targetUser._id.toString());
+
+  return res.status(201).send("Invitation has been sent");
 
 }
 
--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     mail: { type : String , unique: true},
     username : { type : String},
-    password : { type : String}
+    password : { type : String},
+    friends: [{ type: Schema.Types.Object, ref: "User" }],
+
 })
 
 
